17dd844795b2181c21f40380acd7715633e533d7
Oz <perezfloreso@sou.edu>
2026-04-15 20:31:19 -0700

    added multiple creatures to arena

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "arena.h"
 #include "creature.h"
+#include <cstdlib>
+#include <ctime>
+
+
 
 int main()
 {
+    srand(time(0)); // begins random
     Creature c1("Alpha", 100, 15);
     Creature c2("Beta", 100, 12);
 
