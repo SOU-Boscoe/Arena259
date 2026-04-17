a895ce189f21a6689867f6b8e13c8af64e69083c
grumpywizardstudent <klein@sou.edu>
2026-04-16 20:47:55 -0700

    CoinFlip and Color > + Added utils.h > + new constants for generating random numbers > + flipCoin() which is used by Arena to randomize creature turn order > + new enum class Color which facilitates both a mutator method and an operator overload method of colorizing text in the terminal readout > + colorPicker() returns a random color from a specified list. Currently unimplemented, but might be useful later.
    Arena Turn Abstraction
    > + Abstracted the turn logic into a second static function in arena.h and implemented in arena.cpp. Prevents duplication of logic across nested conditionals now that turn order is not static.
    
    Viper Balance Tweak
    > + A trivial value tweak to the makeViper factory to make its competition with GiantLeech more fair.
    
    Improved Battle Readout
    > + Some small additions/modifications to how Arena prints battle results to screen (in addition to adding color)
    
    Documentation
    > + Added verbose documentation to arena.h to help describe the new logic/functionality.

--- src/arena.cpp
+++ src/arena.cpp
@@ -1,36 +1,82 @@
 #include <iostream>
+#include "utils.h"
 #include "arena.h"
 
-void Arena::battle(Creature &a, Creature &b)
+bool Arena::takeTurn(Creature &acting, Creature &target) 
+    /// @brief Abstracts the turn logic into a function to prevent code duplication across nested conditionals going forward.
+    /// @param acting Reference to the creature instance that is acting on the current turn
+    /// @param target Reference to the acting creature's target
+    /// @return returns the inverse of target.isAlive() to pass into the while loop condition hasWinner. 
+    /// @return The truth tables of these values are semantically opposed and should be unified eventually.
+
 {
+    std::cout << acting.name << " attacks for " << Color::YELLOW << acting.damage << Color::RESET << std::endl;
+    acting.attack(target);
+    std::cout << target.name << " health: " << target.health << std::endl;
+
+    return !target.isAlive();
+}
+
+
+void Arena::battle(Creature &a, Creature &b)
+/// @brief Runs the primary loop for Arena battles.
+/// @param a Reference to an instance of Creature
+/// @param b Reference to an instance of Creature
+{
+    /// Wrap the creature names with color values and store them to
+    /// make the output more readable and fun!
+    a.name = colorMe(a.name, Color::BLUE);
+    b.name = colorMe(b.name, Color::GREEN);
+
+    std::cout << "\n================================\n";
     std::cout << a.name << " vs " << b.name << std::endl;
+    std::cout << "================================";
 
     int turn = 1;
 
-    while (a.isAlive() && b.isAlive())
+    /// set hasWinner to false before the while loop
+    /// if takeTurn returns true, we break the loop and
+    /// announce the winner.
+    bool hasWinner = false;
+
+    while (!hasWinner) /// Primary game loop
     {
+        /// Announce turn number and then flip a coin to se
+        /// which creature goes first.
         std::cout << "\nTurn " << turn << std::endl;
+        if (flipCoin()) {
+            /// optional statement for debugging to show who won the coin toss
+            // std::cout << "Heads! " << b.name << " goes first!" << std::endl; 
 
-        std::cout << a.name << " attacks!" << std::endl;
-        a.attack(b);
-        std::cout << b.name << " health: " << b.health << std::endl;
+            hasWinner = takeTurn(b, a); if (hasWinner) {break;}
+            hasWinner = takeTurn(a, b); if (hasWinner) {break;}
+        }
 
-        if (!b.isAlive())
-            break;
+        else {
+            /// optional statement for debugging to show who won the coin toss
+            // std::cout << "Tails! " << a.name << " goes first!" << std::endl;
 
-        std::cout << b.name << " attacks!" << std::endl;
-        b.attack(a);
-        std::cout << a.name << " health: " << a.health << std::endl;
+            hasWinner = takeTurn(a, b); if (hasWinner) {break;}
+            hasWinner = takeTurn(b, a); if (hasWinner) {break;}
+        }
 
         turn++;
     }
 
+
+    /// When the while loop breaks, we need to check to 
+    /// see which creature is still alive and announce 
+    /// them as the winner.
     if (a.isAlive())
     {
+        std::cout << "\n================================\n";
         std::cout << a.name << " wins!" << std::endl;
+        std::cout << "================================\n\n";
     }
     else
     {
+        std::cout << "\n================================\n";
         std::cout << b.name << " wins!" << std::endl;
+        std::cout << "================================\n\n";
     }
 }
--- src/creatures/viper.cpp
+++ src/creatures/viper.cpp
@@ -3,7 +3,7 @@
 Creature makeViper()
 {
     std::string n = "Viper";
-    int h = 80;
+    int h = 90;
     int d = 20;
     return Creature(n, h, d);
 }
