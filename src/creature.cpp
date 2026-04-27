28d04cc7d1aaa3062d85bc03ddb058528013d981
EJS <ethanschilder35@gmail.com>
2026-04-27 08:51:03 +0000

    Lab 2: Defensive measures for all!

---
+++ src/creature.cpp
@@ -0,0 +1,4 @@
+#include "creature.h"
+//File is for static member varible. Not sure if I could do this in creature.h so following
+// what I've seen online hope this is okay!
+int Creature::creatureCount = 0; // Initialize static member variable
