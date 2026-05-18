432d81fec9bd7c85de188ea156ec0861e7cbc50e
Ozvaldo Perez-Flores <perezfloreso@sou.edu>
2026-05-17 18:04:13 -0700

    Hopefully this fixes everything in git hub

--- src/creatures/devil.cpp
+++ src/creatures/devil.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include "devil.h"
 
-LordDevilious::LordDevilious()
+LordDevilious::LordDevilious() // constructor that initializes the base class with specific values for name, health, and damage
     : Creature("Lord Devilious the 666", 120, 10)
 {
 }
 
-void LordDevilious::specialMove(Creature& other)
+void LordDevilious::specialMove(Creature& other) // override the base class special move with a unique one for this creature
 {
-    std::cout << getName()
+    std::cout << getName() // print out the name of the creature using the special move
               << " uses Hellfire Drain!"
               << std::endl;
 
     other.takeDamage(20);
 
-    std::cout << getName()
+    std::cout << getName() // drain health from the target
               << " drains dark energy from the target!"
               << std::endl;
 }
--- src/creatures/devil.h
+++ src/creatures/devil.h
@@ -1,14 +1,14 @@
 #ifndef DEVIL_H
 #define DEVIL_H
 
-#include "../creature.h"
+#include "../creature.h" // include the base class header
 
-class LordDevilious : public Creature
+class LordDevilious : public Creature 
 {
 public:
     LordDevilious();
 
-    void specialMove(Creature& other);
+    void specialMove(Creature& other); // override the base class special move with a unique one for this creature
 };
 
 #endif
